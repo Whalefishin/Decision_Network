8743abae065feef4089d6a8883ac31ba35ff02df
hhuang2 <hhuang2@swarthmore.edu>
2018-06-11 18:34:31 -0400

    parallelize

--- main.cpp
+++ main.cpp
@@ -2,10 +2,79 @@
 #include <iostream>
 #include <utility>
 #include <ctime>
+#include <thread>
+#include <mutex>
 
 #include "Network.h"
 
 using namespace std;
+
+mutex mtx;
+
+struct Data{
+    double N;
+    int update_times;
+    int num_inner_loop_ult;
+    int diff_count;
+    int gain_type;
+    int sep_gain;
+    int normalization;
+    int biased_IC;
+    vector<double> diff_vector;
+    vector<int> IC_vector;
+};
+
+void workLoop(Data custom_data, vector<vector<double> >& W_N_N_Vector,
+    vector<vector<double> >& W_N_W_Vector,
+    vector<vector<double> >& W_N_diff_Vector,
+    vector<vector<double> >& W_N_Acc_Vector,
+    vector<vector<double> >& W_N_RT_Vector){ 
+
+    for (int j=1;j<=custom_data.num_inner_loop_ult;j++){
+        double W = (double)(j-1);
+        int index = 0;
+        for (int d=0;d<custom_data.diff_count;d++){
+            double diff_ult = custom_data.diff_vector[d];
+            for (int g=0;g<custom_data.gain_type;g++){
+                for (int s=0;s<custom_data.sep_gain;s++){
+                    for (int n=0;n<custom_data.normalization;n++){
+                        for (int b=0;b<custom_data.biased_IC;b++){
+                            int num_IC = custom_data.IC_vector[b];                                //double w = 1.0;
+                            double Acc_sum =0; //IC avg.
+                            double RT_sum = 0; //IC avg.
+                            for (int l=1;l<=num_IC;l++){
+                                double IC = 0.1 + 0.8/num_IC * l;
+                                Network network_3D(custom_data.N,W,1,0.1,n);
+                                network_3D.constructAllToAllNetwork();
+                                network_3D.initializeWithChoice(b,IC,diff_ult);
+                                for (int k = 0;k<custom_data.update_times;k++){
+                                    network_3D.updateWithChoice(s,g);
+                                    if (k==custom_data.update_times-1){
+                                        network_3D.computeAccuracy();
+                                    }
+                                }
+                                Acc_sum += max(0,network_3D.Acc); //if Acc is neg, set to 0.
+                                RT_sum += network_3D.RT;                 
+                            }
+                            //data collection
+                            //int index = b*1 + n*2 + s*4 + g*8;
+                            mtx.lock();
+                            W_N_N_Vector[index].push_back(custom_data.N);
+                            W_N_W_Vector[index].push_back(W);
+                            W_N_diff_Vector[index].push_back(diff_ult);
+                            W_N_Acc_Vector[index].push_back(Acc_sum/num_IC);
+                            W_N_RT_Vector[index].push_back(RT_sum/num_IC);
+                            mtx.unlock();
+                    //update index out of the simulation loop
+                            index++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 int main(){
     //fixed random seed for consistency
@@ -92,9 +161,9 @@ int main(){
 
 
     //3D plots
-    int num_outer_loop = 10;
-    int num_inner_loop = 21;
-    int update_times_3D = 10;
+    int num_outer_loop = 30;
+    int num_inner_loop = 11;
+    int update_times_3D = 500;
 
     vector<double> w_Vector_3D;
     vector<double> N_Vector_3D;
@@ -105,7 +174,7 @@ int main(){
     for (int i=1;i<=num_outer_loop;i++){
       cout << "big loop: " + to_string(i) << endl;
         //double N = 10;
-        double N = i*10;
+        double N = i*1+1;
         //double diff = (i-1)*0.1;
         double diff = 0.5;
         for (int j=1;j<=num_inner_loop;j++){
@@ -158,7 +227,7 @@ int main(){
                 //double IC = l*0.09;
                 //double w = 1.0;
                 //double diff = (j-1)*0.1;
-                Network network_3D(N,w/(N-1),1,0.01);
+                Network network_3D(N,w/(N-1),1,0.1);
                 network_3D.constructAllToAllNetwork();
                 network_3D.initializeFairIC(IC,diff);
                 //network_3D.initialize(0.5);
@@ -191,7 +260,7 @@ int main(){
 
     int num_outer_loop_ult = 20;
     int num_inner_loop_ult = 11;
-    int update_times_ult = 1;
+    int update_times_ult = 5;
     //double diff_ult = 0.5;
     vector<double> diff_vector;
     diff_vector.push_back(0.2);
@@ -219,6 +288,7 @@ int main(){
         W_N_RT_Vector.push_back(toPush);
     }
 
+    /*
     int index = 0;
     for (int d=0;d<diff_count;d++){
         double diff_ult = diff_vector[d];
@@ -238,7 +308,7 @@ int main(){
                                 for (int l=1;l<=num_IC;l++){
                                     double IC = 0.1 + 0.8/num_IC * l;
                                     //cout << "here" <<endl;
-                                    Network network_3D(N,W,1,0.01,b);
+                                    Network network_3D(N,W,1,0.1,n);
                                     network_3D.constructAllToAllNetwork();
                                     network_3D.initializeWithChoice(b,IC,diff_ult);
                                     //network_3D.initialize(0.5);
@@ -267,7 +337,36 @@ int main(){
             }
         }
     }
+    */
 
+   
+
+   //Parallel version
+    int index = 0;
+    vector<thread> threads;
+
+    Data computing_data;
+
+    for (int i=1;i<=num_outer_loop_ult;i++){
+        double N = i*10;
+        computing_data.N = N;
+        computing_data.update_times = update_times_ult;
+        computing_data.num_inner_loop_ult = num_inner_loop_ult;
+        computing_data.diff_count = diff_count;
+        computing_data.gain_type = gain_type;
+        computing_data.sep_gain = sep_gain;
+        computing_data.normalization = normalization;
+        computing_data.biased_IC = biased_IC;
+        computing_data.diff_vector = diff_vector;
+        computing_data.IC_vector = IC_vector;
+
+        threads.push_back(thread(workLoop,computing_data,ref(W_N_N_Vector),ref(W_N_W_Vector),
+        ref(W_N_diff_Vector),ref(W_N_Acc_Vector),ref(W_N_RT_Vector)));
+    }
+
+    for (int i=0;i<threads.size();i++){
+        threads[i].join();
+    }
 
 
     //Behold again, the ultimate version for W vs. Diff
@@ -323,7 +422,7 @@ int main(){
                                 for (int l=1;l<=num_IC;l++){
                                     double IC = 0.1 + 0.8/num_IC * l;
                                     //cout << "here" <<endl;
-                                    Network network_3D(N,W,1,0.01,b);
+                                    Network network_3D(N,W,1,0.1,n);
                                     network_3D.constructAllToAllNetwork();
                                     network_3D.initializeWithChoice(b,IC,diff_ult);
                                     //network_3D.initialize(0.5);
